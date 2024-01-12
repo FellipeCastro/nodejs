60ee61208886add30e43cc0688a20943286985d2
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-12 17:54:41 -0300

    aula 13 finalizada

--- teste.sql
+++ teste.sql
@@ -3,3 +3,21 @@ CREATE TABLE usuarios(
     email VARCHAR(100),
     idade INT
 );
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Fellipe Castro", 
+    "fellipecastro@gmail.com", 
+    16
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "João Silva", 
+    "joaosilva@gmail.com", 
+    18
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Ana Maria", 
+    "anamaria@gmail.com", 
+    20
+);
