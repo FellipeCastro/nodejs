7cba193447b1039e7110c198fccc0ca5c3812cfd
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-13 10:54:58 -0300

    aula 20 finalizada

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars")
+const Sequelize = require("sequelize")
 
 // Config  
     // Tamplate Engine
-        app.engine("handlebars", handlebars({defaultLayout: "main"}))
+        app.engine("handlebars", handlebars({
+            defaultLayout: "main",
+            layoutsDir: __dirname + "/views/layouts" 
+        }));
         app.set("view engine", "handlebars")
-    // Conexão com o banco de dados MySQL
-        const Sequelize = require("sequelize")
+    // Conexão com o banco de dados MySQL        
         const sequelize = new Sequelize("teste", "root", "Fsc#1531", {
             host: "localhost",
             dialect: "mysql"
         })
+// Rotas
+    app.get("/cad", (req, res) => {
+        res.render("formulario")
+    })
 
-
-app.listen(8081, function() {
+app.listen(8081, () => {
     console.log("Servidor rodando")
 }) // Ultima linha do código
