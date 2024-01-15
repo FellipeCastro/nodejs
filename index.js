9e4ead02ebbf5397516245163c82e87d140452ca
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-15 19:31:16 -0300

    aula 22 finalizada

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars")
+const bodyParser = require("body-parser")
 const Sequelize = require("sequelize")
 
 // Config  
     // Tamplate Engine
-        app.engine("handlebars", handlebars({
-            defaultLayout: "main",
-            layoutsDir: __dirname + "/views/layouts" 
-        }));
+        app.engine("handlebars", handlebars({ defaultLayout: "main" }))
         app.set("view engine", "handlebars")
+    
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
     // Conexão com o banco de dados MySQL        
         const sequelize = new Sequelize("teste", "root", "Fsc#1531", {
             host: "localhost",
@@ -20,7 +22,8 @@ const Sequelize = require("sequelize")
         res.render("formulario")
     })
     app.post("/add", (req, res) => {
-        res.send("Formulario recebido")
+        // Pegando dados do formulario
+        res.send("Texto: " + req.body.titulo + "<br> Conteúdo: " + req.body.conteudo)
     })
 
 app.listen(8081, () => {
