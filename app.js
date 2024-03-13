405d73c4a1d81e6199e89d2330153ef9ebcf7eb6
FellipeCastro <fellipecastro27d@gmail.com>
2024-03-13 18:18:17 -0300

    aula 32 finalizada

--- app.js
+++ app.js
@@ -4,22 +4,26 @@ const exphbs = require("express-handlebars")
 const handlebars = exphbs.create({})
 const bodyParser = require("body-parser")
 const app = express()
+const admin = require('./routes/admin.js')
 // const mongoose = require("mongoose")
 
 // Configurações
-    // Body Parser
-    app.use(bodyParser.urlencoded({extended: true}))
-    app. use(bodyParser.json())
-    // Handlebars
-    app.engine('handlebars', handlebars.engine)
-    app.set('view engine', 'handlebars')
-    // Mongoose
+
+// Body Parser
+app.use(bodyParser.urlencoded({extended: true}))
+app. use(bodyParser.json())
+
+// Handlebars
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+
+// Mongoose
 
 // Rotas
-
+app.use('/admin', admin)
 
 // Outros
 const PORT = 8081
 app.listen(PORT, () => {
-    console.log("Servidor rodando")
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('PAGINA PRINCIPAL')
+})
+
+router.get('/posts', (req, res) => {
+    res.send('POSTS')
+})
+
+router.get('/categorias', (req, res) => {
+    res.send('CATEGORIAS')
+})
+
+module.exports = router
