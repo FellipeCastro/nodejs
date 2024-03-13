040acea2dc982b291d48f6cf5672b12a70b529de
FellipeCastro <fellipecastro27d@gmail.com>
2024-03-13 18:04:08 -0300

    aula 31 finalizada

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Carregando Módulos
 const express = require("express")
+const exphbs = require("express-handlebars")
+const handlebars = exphbs.create({})
 const bodyParser = require("body-parser")
 const app = express()
 // const mongoose = require("mongoose")
@@ -9,8 +11,8 @@ const app = express()
     app.use(bodyParser.urlencoded({extended: true}))
     app. use(bodyParser.json())
     // Handlebars
-    app.engine("handlebars", require("express-handlebars")({defaultLayout: "main"}))
-    app.set("view engine", "handlebars")
+    app.engine('handlebars', handlebars.engine)
+    app.set('view engine', 'handlebars')
     // Mongoose
 
 // Rotas
