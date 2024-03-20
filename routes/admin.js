08577b7df0995aae57f15e8026564c5ca04c342b
FellipeCastro <fellipecastro27d@gmail.com>
2024-03-20 17:50:23 -0300

    aula 48 finalizada

--- routes/admin.js
+++ routes/admin.js
@@ -178,4 +178,46 @@ router.post('/postagens/nova', (req, res) => {
     }
 })
 
+router.get('/postagens/edit/:id', (req, res) => {
+
+    Postagem.findOne({ _id: req.params.id }).then((postagem) => {
+
+        Categoria.find().then((categorias) => {
+            res.render('admin/editpostagens', { categorias: categorias, postagem: postagem })
+        }).catch((err) => {
+            req.flash('error_msg', 'Erro ao listar categorias')
+            res.redirect('/admin/postagens')
+            console.log(err)
+        })
+
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao carregar formulário de edição')
+        res.redirect('/admin/postagens')
+        console.log(err)
+    })
+})
+
+router.post('/postagens/edit', (req, res) => {
+    Postagem.findOne({ _id: req.body.id }).then((postagem) => {
+        postagem.titulo = req.body.titulo
+        postagem.slug = req.body.slug
+        postagem.descricao = req.body.descricao
+        postagem.conteudo = req.body.conteudo
+        postagem.categoria = req.body.categoria
+
+        postagem.save().then(() => {
+            req.flash('success_msg', 'Postagem editada com sucesso')
+            res.redirect('/admin/postagens')
+        }).catch((err) => {
+            req.flash('error_msg', 'Erro ao editar postagem')
+            res.redirect('/admin/postagens')
+            console.log(err)
+        })
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao editar postagem')
+        res.redirect('/admin/postagens')
+        console.log(err)
+    })
+})
+
 module.exports = router
